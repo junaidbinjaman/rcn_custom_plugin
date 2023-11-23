6caa2e74ec23cf57d0998535603419f71c9730af
Junaid Bin Jaman <junaidbinjaman@Junaids-MacBook-Pro.local>
2023-11-23 18:08:19 +0600

    First commit

---
+++ includes/class-rcn-deactivator.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://junaidbinjaman.com
+ * @since      1.0.0
+ *
+ * @package    Rcn
+ * @subpackage Rcn/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Rcn
+ * @subpackage Rcn/includes
+ * @author     Junaid Bin Jaman <me@junaidbinjaman.com>
+ */
+class Rcn_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+	}
+}
